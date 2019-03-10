b3dff8dfae9fdbb2f4ba5b1628d60d8e849e5e2e
JeremyOlea <43157357+JeremyOlea@users.noreply.github.com>
2019-03-10 14:05:25 -0600

    Completed

--- app.js
+++ app.js
@@ -1,17 +1,88 @@
+
+let total = null;
+let numA = null;
+let numB = null;
+let operation = null;
+
+let myButtons = document.querySelectorAll('button');
+let display = 0;
+
+myButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        calculate(button);
+    });
+});
+
+function calculate(input) {
+    if(input.className == 'button number') {
+        if(numA == null) {
+            numA = input.textContent;
+            display = numA;
+
+        }
+        else if(operation == null) {
+            numA += input.textContent;
+            display = numA;
+
+        } 
+        else {
+            if(numB == null) {
+                numB = input.textContent;
+                display = numB;
+
+            }
+            else {
+                numB += input.textContent;
+                display = numB;
+ 
+            }
+        }
+    }
+    else if(input.className == 'button operator') {
+        if(numA != null && numB != null && operation != null) {
+            if(total == null)
+                total = operate(numA, numB, operation);
+            else
+                total = operate(total, numB, operation);
+
+            numB = null;
+            display = total;
+        }
+
+        operation = input.id;
+    }
+    else if(input.id == 'clear') {
+        clear();
+    }
+    
+    document.getElementById('value').textContent = display.toString(10);
+    if(display == 'Error') clear();
+    console.log(display);
+}
+
+function clear() {
+    total = null;
+    numA = null;
+    numB = null;
+    operation = null;
+    display = 0;
+}
+
 function add(a,b) {
-    return a+b;
+    return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a,b) {
-    return a-b;
+    return parseFloat(a) - parseFloat(b);
 }
 
 function multiply(a,b) {
-    return a*b;
+    return parseFloat(a) * parseFloat(b);
 }
 
 function divide(a,b) {
-    return a/b;
+    if(b == '0') return 'Error';
+    return parseFloat(a) / parseFloat(b);
 }
 
 function operate(numA, numB, operation) {
@@ -34,8 +105,11 @@ function operate(numA, numB, operation) {
             result = divide(numA, numB);
             break;
 
+        case 'equals':
+            result = numA;
+            break;
+
         default:
     }
     return result;
 }
-
